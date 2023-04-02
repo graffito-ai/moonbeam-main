ba6d7d620c9663a77372bea0add5f9dff28fd797
Marius Acsinte <marius@moonbeam.vet>
2023-04-01 23:52:27 -0700

    feat(moonbeam-app): #18 - finishes adding the FAQ resolvers.
    MOONBM-18

--- packages/moonbeam-faq-lambda/lib/types/lambda/resolvers/createFAQ.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateFaqInput, FaqResponse } from "@moonbeam/moonbeam-models";
-/**
- * CreateFAQ resolver
- *
- * @param createFaqInput object to be used when creating a new FAQ object
- * @returns {@link Promise} of {@link FaqResponse}
- */
-export declare const createFAQ: (createFaqInput: CreateFaqInput) => Promise<FaqResponse>;
--- packages/moonbeam-faq-lambda/lib/types/lambda/resolvers/listFAQs.d.ts
+++
@@ -1,8 +0,0 @@
-import { FaqResponse, ListFaqInput } from "@moonbeam/moonbeam-models";
-/**
- * ListFAQs resolver
- *
- * @param listFaqInput input to be passed in, which will help filter through all the FAQs
- * @returns {@link Promise} of {@link AccountResponse}
- */
-export declare const listFAQs: (listFaqInput: ListFaqInput) => Promise<FaqResponse>;
